28e1fedd7611f341ce5eb8bad911998f95d8690a
Anyihh <anyelenilara@gmail.com>
2023-12-20 01:19:59 -0500

    feat: Students table can now filter data

--- src/routes/academix/students/+page.svelte
+++ src/routes/academix/students/+page.svelte
@@ -1,24 +1,54 @@
 <script>
-    import Table from '$lib/components/shared/Table.svelte';
-    import { onMount } from 'svelte';
-    import studentService from '$lib/services/StudentService.js';
-    import { page } from '$app/stores';
+	import Table from '$lib/components/shared/Table.svelte';
+	import { onMount } from 'svelte';
+	import studentService from '$lib/services/StudentService.js';
+	import studentsGroupService from '$lib/services/StudentsGroupService.js';
+	import yearService from '$lib/services/YearService.js';
+	import academicSituationService from '$lib/services/AcademicSituationService.js';
+	import { currentSchoolYear } from '$lib/stores/stores.js';
+	import { page } from '$app/stores';
 
-    onMount(() => {
+	onMount(async () => {
+		let yearServ = yearService.getInstance();
+		let academicSituationServ = academicSituationService.getInstance();
+		let studentsGroupServ = studentsGroupService.getInstance();
+		years = await yearServ.getYears();
+		years = years.filter(({ school_year }) => school_year === $currentSchoolYear);
+		years = years.map(({ year }) => year);
+		filters.find((filter) => filter.key === 'year').options = years;
+		groups = await studentsGroupServ.getStudentsGroups();
+		filters.find((filter) => filter.key === 'group_number').options = groups.map(
+			({ group_number }) => group_number
+		);
+		filters.find((filter) => filter.key === 'group_number').options = filters
+			.find((filter) => filter.key === 'group_number')
+			.options.filter((group, index, self) => index === self.findIndex((t) => t === group));
+		academicSituations = await academicSituationServ.getAcademicSituations();
+		academicSituations = academicSituations.map(
+			({ academic_situation_name }) => academic_situation_name
+		);
+		filters.find((filter) => filter.key === 'academic_situation').options = academicSituations;
 		refreshItems();
 	});
 
 	let students = [],
 		filters = [
-			{ name: 'Situaciones Académicas', key: 'academicSituation', options: [], selectedOptions: [] },
-			{ name: 'Años', key: 'academicYear', options: [], selectedOptions: [] },
-			{ name: 'Grupos', key: 'group', options: [], selectedOptions: [] }
+			{
+				name: 'Situaciones Académicas',
+				key: 'academic_situation',
+				options: [],
+				selectedOptions: []
+			},
+			{ name: 'Años', key: 'year', options: [], selectedOptions: [] },
+			{ name: 'Grupos', key: 'group_number', options: [], selectedOptions: [] }
 		],
-		isFilterable = true,
+		years,
+		groups,
+		academicSituations;
+	let isFilterable = true,
 		isCreatable = false,
 		isUpdatable = false,
 		isDeletable = false;
-
 	let tableName = 'Estudiantes';
 	let studentServ = studentService.getInstance();
 

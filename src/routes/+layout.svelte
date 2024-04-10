4e86d8d6b2f9d4b872ee2eac2e4096dd91a0b7e7
evg1010 <vazquezgarciaeva10@gmail.com>
2024-04-10 10:42:42 -0400

    refactor: change in dark theme colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,23 @@
 	import '../app.css';
 	import { Footer, FooterCopyright } from 'flowbite-svelte';
 	import { t } from '$lib/stores/stores.js';
+
 	$: copyrightMessage = $t('Todos los derechos reservados') + '';
 </script>
 
 <div
 	class="min-h-screen min-w-max bg-gradient-to-tl from-rose-300 via-rose-200 to-rose-100
-	dark:from-primary-950 dark:to-primary-900"
+	dark:from-pink-950 dark:to-gray-900"
 >
 	<slot />
 	<div class="fixed bottom-0 w-full">
 		<Footer>
 			<FooterCopyright
-				spanClass="block mb-1 text-sm font-semibold sm:text-center text-primary-950 dark:text-primary-100"
-				href="/"
 				by="Academix™"
-				year={2023}
 				{copyrightMessage}
+				href="/"
+				spanClass="block mb-1 text-sm font-semibold sm:text-center text-primary-950 dark:text-primary-100"
+				year={2023}
 			/>
 		</Footer>
 	</div>
